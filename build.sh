dcce1a102256b25e00e948482c220000ab2099ce
Ubuntu <ubuntu@ip-172-31-12-72.ap-northeast-2.compute.internal>
2017-08-08 07:58:51 +0000

    fixed ip address

--- build.sh
+++
@@ -1,5 +0,0 @@
-go get github.com/russross/blackfriday
-go get github.com/go-sql-driver/mysql
-go get github.com/gyukebox/gyulog/post
-go get github.com/gyukebox/gyulog/admin
-go build -o bin/application 
