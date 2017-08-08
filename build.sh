a03a78ce5f051461987b0434ddb9e82070b3f1a2
Byeong Gyu Choi <bisschoi9541@gmail.com>
2017-08-08 22:30:05 +0900

    build shell script

---
+++ build.sh
@@ -0,0 +1,5 @@
+go get github.com/russross/blackfriday
+go get github.com/go-sql-driver/mysql
+go get github.com/gyukebox/gyulog/post
+go get github.com/gyukebox/gyulog/admin
+go build -o bin/application 
