b815cc32c241fe28e1948c02046fbb42d0acee3a
Byeong Gyu Choi <bisschoi9541@gmail.com>
2017-08-13 20:23:23 +0900

    removed unnecessary files

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ go get github.com/russross/blackfriday
 go get github.com/go-sql-driver/mysql
 go get github.com/gyukebox/gyulog/post
 go get github.com/gyukebox/gyulog/admin
-go build -o bin/application 
+go build -o bin/application application.go
